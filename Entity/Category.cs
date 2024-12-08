a2e8d24ca76d8765b7d750d17b887c874f8b09fb
e0548547814 <38328178405@mby.co.il>
2024-12-08 16:27:20 +0200

    ff

--- Entity/Category.cs
+++ Entity/Category.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace MyShop.Models;
+namespace Entity;
 
 public partial class Category
 {
--- Entity/User.cs
+++ Entity/User.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace MyShop.Models;
+namespace Entity;
 
 public partial class User
 {
